6c6fc2ec5d247674c2320866cb3d14a7d658af01
xcd <137034181@qq.com>
2023-01-03 23:41:30 +0800

    Initial commit

---
+++ src/main/java/com/doudou/Main.java
@@ -0,0 +1,11 @@
+package com.doudou;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ApplicationContext ctx = new ClassPathXmlApplicationContext("applicationContext.xml");
+        System.out.println(ctx);
+    }
+}
